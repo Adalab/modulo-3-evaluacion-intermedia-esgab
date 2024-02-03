949a2f78d48bc252828917d7cdddbd42ce6759a8
Esther García <garbennes@gmail.com>
2024-02-03 09:00:16 +0100

    Create basic html structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,27 @@ import '../scss/App.scss'
 function App() {
   return (
     <div className="app">
-      Escribe aquí tu código
+      <header className="header">
+        <h1>Frases de Friends</h1>
+        <form>
+        <label for="phraseFilter">Filtrar por frase:</label>
+        <input type="text" id="phraseFilter" name="phraseFilter" />
+        <label for="filtroPersonaje">Filtrar por personaje:</label>
+        <select id="characterFilter" name="characterFilter">
+          <option value="ross">Ross</option>
+          <option value="rachel">Rachel</option>
+          <option value="monica">Monica</option>
+          <option value="chandler">Chandler</option>
+          <option value="joey">Joey</option>
+          <option value="phoebe">Phoebe</option>
+        </select>
+      </form>
+      </header>
+      <main>
+        <ul>
+          <li>Pivot! Pivot! Pivot! Pivot! Pivot! - <span>Ross</span></li>
+        </ul>
+      </main>
     </div>
   )
 }
